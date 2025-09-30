c78c1818ad5ecf4149fd1999e37102e908af82c8
Jbuican19 <jbuican19@gmail.com>
2025-09-30 19:50:48 +0100

    add condition for button and product list condition

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -1,12 +1,14 @@
 import './ProductListItem.css';
 
-const ProductListItem = ({name, price, imageUrl, onAddToCart}) => {
+const ProductListItem = ({name, price, imageUrl, onAddToCart, isSoldOut}) => {
   return (
     <div className="card">
         <h2>{name}</h2>
         <img src={imageUrl} alt={name} className="card-img-top" />
         <small>{price}</small>
-        <button onClick={onAddToCart}>Add to Cart</button>  
+        <button onClick={onAddToCart} disabled={isSoldOut}>
+          {isSoldOut ? 'Sold Out' : 'Add to Cart'}
+        </button>  
     </div>
   );
 };
